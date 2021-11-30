315990ef9e7c74e32ea6b4358de5ec22437544e1
legator-admin <hello@legatorsolutions.com>
2021-12-01 00:24:51 +0530

    tree map modify

--- component/treemap/treemap.js
+++ component/treemap/treemap.js
@@ -4,6 +4,22 @@ import * as d3 from 'd3'
 
 const TreeMap = ({data}) => {
 
+    const NumberPattern = (num) =>{
+        return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")+'.00'
+    }
+
+    const RemarkGenerator = (am,pm,type_amount) =>{
+        let rtn = ''
+        if (am < pm){
+            rtn = 'PM '+type_amount+' is more than AM'
+        }else if(am > pm){
+            rtn = 'PM '+type_amount+' is less than AM'
+        }else{
+            rtn = 'PM '+type_amount+' is less equal AM'
+        }
+        return rtn
+    }
+
     const AnalyticsGenerator = (array) => {
         
         let AMcredit = 0.00,
@@ -44,25 +60,25 @@ const TreeMap = ({data}) => {
             name: 'Analytics' , 
             label:{fontSize: 14,fontWeight: 'bold'},
             children:[
-                {name:'Credit('+(AMcredit+PMcredit)+' , '+(amCreditVoucher+pmCreditVoucher)+')',
+                {name:'Credit('+NumberPattern(AMcredit+PMcredit)+' , '+(amCreditVoucher+pmCreditVoucher)+')',
                  label:{fontSize: 14,fontWeight: 'bold'},
                  lineStyle:{color:'#DC143C'},
                 children: [
-                    {name:'AM('+AMcredit+','+amCreditVoucher+')', label:{fontSize: 14,fontWeight: 'bold'}},
-                    {name:'PM('+PMcredit+','+pmCreditVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
-                    {name:'Remark('+((PMcredit>= AMcredit)? 'PM Credit is more than AM':'AM Credit is more than PM')+')', label:{fontSize: 14,fontWeight: 'bold'},lineStyle:{color:'#DC143C'},}
+                    {name:'AM('+NumberPattern(AMcredit)+','+amCreditVoucher+')', label:{fontSize: 14,fontWeight: 'bold'}},
+                    {name:'PM('+NumberPattern(PMcredit)+','+pmCreditVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
+                    {name:'Remark('+RemarkGenerator(AMcredit,PMcredit,'Credit')+')', label:{fontSize: 14,fontWeight: 'bold'},lineStyle:{color:'#DC143C'},}
                 ]
                 },
-                {name:'Debit('+(AMdebit+PMdebit)+' , '+(amDebitVoucher+pmDebitVoucher)+')',
+                {name:'Debit('+NumberPattern(AMdebit+PMdebit)+' , '+(amDebitVoucher+pmDebitVoucher)+')',
                  label:{fontSize: 14,fontWeight: 'bold' },
                  lineStyle:{color:'#DC143C'},
                 children: [
-                    {name:'AM('+AMdebit+','+amDebitVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
-                    {name:'PM('+PMdebit+','+pmDebitVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
-                    {name:'Remark('+((PMdebit <= AMdebit)? 'PM Debit is more than AM':'AM Debit is more than PM')+')', label:{fontSize: 14,fontWeight: 'bold'},lineStyle:{color:'#DC143C'}}
+                    {name:'AM('+NumberPattern(AMdebit)+','+amDebitVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
+                    {name:'PM('+NumberPattern(PMdebit)+','+pmDebitVoucher+')',label:{fontSize: 14,fontWeight: 'bold'}},
+                    {name:'Remark('+RemarkGenerator(AMdebit,PMdebit,'Debit')+')', label:{fontSize: 14,fontWeight: 'bold'},lineStyle:{color:'#DC143C'}}
                 ]
                 },
-                {name:'Diff('+(AMcredit+PMcredit+AMdebit+PMdebit)+' , '+Math.abs((amCreditVoucher+pmCreditVoucher)-(amDebitVoucher+pmDebitVoucher))+')',
+                {name:'Diff('+NumberPattern(AMcredit+PMcredit+AMdebit+PMdebit)+' , '+Math.abs((amCreditVoucher+pmCreditVoucher)-(amDebitVoucher+pmDebitVoucher))+')',
                     label:{fontSize: 14,fontWeight: 'bold'},
                     lineStyle:{color:'#DC143C'},
                 }
@@ -82,76 +98,69 @@ const TreeMap = ({data}) => {
 
     const user_analytics = (arr) => {
 
-        const groupBy_trans = Array.from(d3.group(arr, d => d['IC4PROTRANSTYPE']), ([name,children]) => ({name,children})).map(d1=>{
-            const data =  AnalyticsGenerator(d1.children)
-            const AM = {name:'AM: '+(data.children[0].children[0].name).replace('AM','Cr')+' '+(data.children[1].children[0].name).replace('AM','Dr'), 
-                label:data.children[0].label}
-            const PM = {name:'PM: '+(data.children[0].children[1].name).replace('PM','Cr')+' '+(data.children[1].children[1].name).replace('PM','Dr'), 
-            label:data.children[0].label}
-            //     credit_AM.name = 'AM:'+credit_AM.name
-            // let credit_PM =  AnalyticsGenerator(d1.children).children[0].children[1]
-            //     credit_PM = 'Cr:'+credit_PM.name
-            return{
-                name:d1.name,
-                children:[
-                   AM,
-                   PM,
+        // const groupBy_trans = Array.from(d3.group(arr, d => d['IC4PROTRANSTYPE']), ([name,children]) => ({name,children})).map(d1=>{
+        //     const data =  AnalyticsGenerator(d1.children)
+        //     const AM = {name:'AM: '+(data.children[0].children[0].name).replace('AM','Cr')+' '+(data.children[1].children[0].name).replace('AM','Dr'), 
+        //         label:data.children[0].label}
+        //     const PM = {name:'PM: '+(data.children[0].children[1].name).replace('PM','Cr')+' '+(data.children[1].children[1].name).replace('PM','Dr'), 
+        //     label:data.children[0].label}
+        //     //     credit_AM.name = 'AM:'+credit_AM.name
+        //     // let credit_PM =  AnalyticsGenerator(d1.children).children[0].children[1]
+        //     //     credit_PM = 'Cr:'+credit_PM.name
+        //     return{
+        //         name:d1.name,
+        //         children:[
+        //            AM,
+        //            PM,
                     
-                ]
-            }
-        })
-        const groupBy_acc = Array.from(d3.group(arr, d => d['IC4PROACCOUNTTYPE']), ([name,children]) => ({name,children})).map(d2=>{
+        //         ]
+        //     }
+        // })
+
+        const groupBy_trans = Array.from(d3.group(arr, d => d['IC4PROTRANSTYPE']), ([name,children]) => ({name,children})).map(d2=>{
+            const tmp = AnalyticsGenerator(d2.children)
             return{
                 name:d2.name,
-                children:AnalyticsGenerator(d2.children).children
+                analytics: tmp,
+                children:[{name:'a'}]
+            }
+        })
+
+        const groupBy_acc = Array.from(d3.group(arr, d => d['IC4PROACCOUNTTYPE']), ([name,children]) => ({name,children})).map(d2=>{
+            console.log(AnalyticsGenerator(d2.children))
+            const tmp = AnalyticsGenerator(d2.children)
+            return{
+                name:d2.name,
+                analytics: tmp,
+                children:d2.children
             }
         })
         
 
         const output = [
             {name:'Trans Type',
-             children: groupBy_trans
+             analytics: groupBy_trans.analytics,
+             children: [{name:'a'}]
             },
             {name:'Account Type',
-             children:groupBy_acc
+             analytics:groupBy_acc.analytics,
+             children: groupBy_acc.children
             }
             
         ]
-        const account_type = {name:'Account Type',
-            children: TreeGenerator('type',arr,'IC4PROACCOUNTTYPE').map(k2=>{
-                //console.log(AnalyticsGenerator(k2.children).children)
-            return{
-                name:k2.name,
-                children: AnalyticsGenerator(k2.children).children
-            }
-        })
-        }
+        // const account_type = {name:'Account Type',
+        //     children: TreeGenerator('type',arr,'IC4PROACCOUNTTYPE').map(k2=>{
+        //         //console.log(AnalyticsGenerator(k2.children).children)
+        //     return{
+        //         name:k2.name,
+        //         children: AnalyticsGenerator(k2.children).children
+        //     }
+        // })
+        // }
         
 
         return output
-        // [
-        //     {name:'Trans Type',
-        //      children: TreeGenerator(d7.name,d7.children,'IC4PROTRANSTYPE').map(d8=>{
-        //          return{
-        //             name:d8.name,
-        //             analytics: AnalyticsGenerator(d8.children),
-        //             children: user_analytics(d8.children)
-        //          }
-        //      })
-        //     },
-        //     {name:'a',children:[{name:'b'}] }
-        //     // {name:'Account Type',
-        //     //  children: TreeGenerator(d7.name,d7.children,'IC4PROACCOUNTTYPE').map(d9=>{
-        //     //     return{
-        //     //        name:d9.name,
-        //     //        analytics:(d9.children != null)?AnalyticsGenerator(d9.children):[],
-        //     //        children: (d9.children != null)?user_analytics(d9.children):[]
-        //     //     }
-        //     // })
-        //     //}
-        // ]
-
-        //return [{name:'a',children:[{name:'b'},{name:'c'}]}]
+        
     }
 
     let treeData = {name:'callover',analytics: AnalyticsGenerator(data), 
@@ -179,7 +188,28 @@ const TreeMap = ({data}) => {
                                                 return{
                                                     name:d7.name,
                                                     analytics: AnalyticsGenerator(d7.children),
-                                                    children: user_analytics(d7.children)
+                                                    children: [
+                                                        {
+                                                            name:'Trans Type',
+                                                            analytics:  AnalyticsGenerator(d7.children),
+                                                            children: TreeGenerator(d7.name,d7.children,'IC4PROTRANSTYPE').map(d8=>{
+                                                                return{
+                                                                    name:d8.name,
+                                                                    children: AnalyticsGenerator(d8.children).children
+                                                                }
+                                                            })
+                                                        },
+                                                        {
+                                                            name:'Account Type',
+                                                            analytics:  AnalyticsGenerator(d7.children),
+                                                            children: TreeGenerator(d7.name,d7.children,'IC4PROACCOUNTTYPE').map(d8=>{
+                                                                return{
+                                                                    name:d8.name,
+                                                                    children: AnalyticsGenerator(d8.children).children
+                                                                }
+                                                            })
+                                                        }
+                                                    ]
 
                                                     // children: TreeGenerator(d7.name,d7.children,'IC4PROTRANSTYPE').map(d8 => {
                                                     //     return{
@@ -201,21 +231,47 @@ const TreeMap = ({data}) => {
     )}
 
     const analytics_manipulation = (arr) => {
-        if(arr.analytics){
-            arr.children.push(arr.analytics)
-            arr.children.forEach(k => {
-                analytics_manipulation(k)
-            });
+
+        if(arr.analytics) arr.children.push(arr.analytics)
+            arr.children.forEach(k1 => {
+                if(k1.analytics) k1.children.push(k1.analytics)
+                    k1.children.forEach(k2 => {
+                        if(k2.analytics) k2.children.push(k2.analytics)
+                        if(k2.children)k2.children.forEach(k3 => {
+                            if(k3.analytics) k3.children.push(k3.analytics)
+                            if(k3.children)k3.children.forEach(k4 => {
+                                if(k4.analytics) k4.children.push(k4.analytics) 
+                                if(k4.children)k4.children.forEach(k5 => {
+                                    if(k5.analytics) k5.children.push(k5.analytics) 
+                                    if(k5.children)k5.children.forEach(k6 => {
+                                       if(k6.analytics){
+                                           //k6.children.push(k6.analytics)
+                                           console.log(k6.children)
+                                           //k6.children.push(k6.analytics)
+                                        //    k6.children.forEach(k7 => {
+                                        //        console.log(k7)
+                                        //    });
+                                       }
+                                    })
+                                })
+                            })
+
+                        })
+                        })
+                    })
+                
+                
             
-        }
+            //     arr.children.forEach(k => {
+            //         analytics_manipulation(k)
+            //     });
+                
+        
+        
     }
     analytics_manipulation(treeData)
 
     console.log(treeData)
-
-    // data.children.forEach(function (datum, index) {
-    //     index % 2 === 0 && (datum.collapsed = true);
-    // });
 
     const option = {
         tooltip: {
